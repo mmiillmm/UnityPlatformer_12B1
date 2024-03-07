3f3596d40fbf25d052fda611cd2c14af6ae0ebc8
Makai Kristóf <kmakai520@gmail.com>
2024-03-07 12:16:13 +0100

    asd

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 5f;
+    [SerializeField] private Slider healthBar;
     private float currentHealth;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
+        healthBar.gameObject.SetActive(true);
+
         currentHealth -= damage;
+        healthBar.value = currentHealth;
         if (currentHealth <= 0) KillMe();
     }
 
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,45 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 20f;
     [SerializeField] private GameObject bloodDropsFX;
     [SerializeField] private float dmgImmunityRate = .5f;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Image bloodScreen;
+    [SerializeField] private float smoothColor = 2f;
 
     private float currentHealth;
     private float nextDamage;
+    private bool isDamaged;
 
     private void Start()
     {
         currentHealth = maxHealth;
         nextDamage = 0f;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+    }
+
+    public void Update()
+    {
+        if (isDamaged)
+        {
+            bloodScreen.color = new(255, 255, 255, 0.8f);
+        }
+
+        else
+        {
+            bloodScreen.color = Color.Lerp(
+                bloodScreen.color,
+                new(255, 255, 255, 0f),
+                Time.deltaTime * smoothColor);
+        }
+
+        isDamaged = false;
+
     }
 
     public void TakeDamage(float damage)
@@ -23,6 +49,10 @@ public class PlayerHealth : MonoBehaviour
             nextDamage = Time.time + dmgImmunityRate;
             Instantiate(bloodDropsFX, transform.position, bloodDropsFX.transform.localRotation);
             currentHealth -= damage;
+            isDamaged = true;
+
+            healthBar.value = currentHealth;
+
 
             if (currentHealth <= 0) MakeDeath();
         }
@@ -31,5 +61,6 @@ public class PlayerHealth : MonoBehaviour
     private void MakeDeath()
     {
         Destroy(gameObject);
+        bloodScreen.color = new(255, 255, 255, 1f);
     }
 }
